e4213f88bd11b4760e0a7cd0c01b78f97666e7b5
davidleon <jd.leon@unimayor.edu.co>
2024-06-12 16:58:23 -0500

    feat: landing layout using in the page and footer, header components

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      This is the marketing page
+      <Button size="default">Click me</Button>
+    </div>
+  );
+}
