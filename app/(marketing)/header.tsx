ebb64462afabb96a49eb2bbfd636d5fc5c57f08e
David leon <jd.leon@unimayor.edu.co>
2024-06-21 12:44:55 -0500

    feat: responsive sidebar is displayed from 1024px onwards and in mobile mode it is displayed as a left side modal.
    It contains buttons that lead to the different pages such as shop, learnIt contains buttons that direct to the different pages such as shop, learn, etc

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -15,7 +15,7 @@ export const Header: React.FC = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-ful">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-3">
-          <Image src="/mascot.jpg" height={40} width={40} alt="Mascot" />
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
             Lingo
           </h1>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,9 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { SidebarItem } from "@/components/sidebar-item";
 
 type Props = {
   className?: string;
@@ -8,11 +13,37 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="/quests.png" />
+        <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+      {/* TODO: The modal for the provider clerk appears, but it doesn't let you click on it It's like a z-index is preventing it or something similar, because in desktop mode it works correctly */}
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
