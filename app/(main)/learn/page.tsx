9c39d46c546f641ab8d5c58aa694c9a32a0d0fbf
David leon <jd.leon@unimayor.edu.co>
2024-06-20 21:23:28 -0500

    feat: responsive design of main template to be used on mobile and portable devices

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,10 @@
+// type Props = {
+//   children: React.ReactNode;
+// };
+
+// const LearnPage = ({ children }: Props) => {
+const LearnPage = () => {
+  return <div>Learn Page in main</div>;
+};
+
+export default LearnPage;
