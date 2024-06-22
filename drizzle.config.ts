fa7dbb3215df3273164dcb9d00f73b360fb70285
David leon <jd.leon@unimayor.edu.co>
2024-06-22 17:10:22 -0500

    feat: The project was initialized in neon and the ORM drizzle was configured, with the connection, basic configuration and the courses table was created

---
+++ drizzle.config.ts
@@ -0,0 +1,25 @@
+// import "dotenv/config";
+// import type { Config } from "drizzle-kit";
+
+// export default {
+//   schema: "./db/schema.ts",
+//   out: "./drizzle",
+//   driver: "pg",
+//   dbCredentials: {
+//     connectionString: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
+
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env" });
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
